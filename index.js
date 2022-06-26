86b11afc655af658cc933744a75a0d91963e208e
tauhid patel <tauhidpatel18@gmail.com>
2022-06-26 20:05:12 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,96 @@
+const image = document.getElementById("image");
+const image2 = document.getElementById("image2");
+const image3 = document.getElementById("image3");
+const image4 = document.getElementById("image4");
+const image5 = document.getElementById("image5");
+const image6 = document.getElementById("image6");
+const image7 = document.getElementById("image7");
+const image8 = document.getElementById("image8");
+const image9 = document.getElementById("image9");
+const image10 = document.getElementById("image10");
+const image11 = document.getElementById("image11");
+const image12 = document.getElementById("image12");
+const image13 = document.getElementById("image13");
+const image14 = document.getElementById("image14");
+const image15 = document.getElementById("image15");
+const image16 = document.getElementById("image16");
+
+fetch('https://picsum.photos/200/300?random')
+.then(data => {
+  image.innerHTML = `<img src=${data.url} />`
+})
+
+fetch('https://picsum.photos/200/300?random')
+.then(data => {
+  image2.innerHTML = `<img src=${data.url} />`
+})
+
+fetch('https://picsum.photos/200/300?random')
+.then(data => {
+  image3.innerHTML = `<img src=${data.url} />`
+})
+
+fetch('https://picsum.photos/200/300?random')
+.then(data => {
+  image4.innerHTML = `<img src=${data.url} />`
+})
+
+fetch('https://picsum.photos/200/300?random')
+.then(data => {
+  image5.innerHTML = `<img src=${data.url} />`
+})
+
+fetch('https://picsum.photos/200/300?random')
+.then(data => {
+  image6.innerHTML = `<img src=${data.url} />`
+})
+
+fetch('https://picsum.photos/200/300?random')
+.then(data => {
+  image7.innerHTML = `<img src=${data.url} />`
+})
+
+fetch('https://picsum.photos/200/300?random')
+.then(data => {
+  image8.innerHTML = `<img src=${data.url} />`
+})
+
+fetch('https://picsum.photos/200/300?random')
+.then(data => {
+  image9.innerHTML = `<img src=${data.url} />`
+})
+
+fetch('https://picsum.photos/200/300?random')
+.then(data => {
+  image10.innerHTML = `<img src=${data.url} />`
+})
+
+fetch('https://picsum.photos/200/300?random')
+.then(data => {
+  image11.innerHTML = `<img src=${data.url} />`
+})
+
+fetch('https://picsum.photos/200/300?random')
+.then(data => {
+  image12.innerHTML = `<img src=${data.url} />`
+})
+
+fetch('https://picsum.photos/200/300?random')
+.then(data => {
+  image13.innerHTML = `<img src=${data.url} />`
+})
+
+fetch('https://picsum.photos/200/300?random')
+.then(data => {
+  image14.innerHTML = `<img src=${data.url} />`
+})
+
+fetch('https://picsum.photos/200/300?random')
+.then(data => {
+  image15.innerHTML = `<img src=${data.url} />`
+})
+
+fetch('https://picsum.photos/200/300?random')
+.then(data => {
+  image16.innerHTML = `<img src=${data.url} />`
+})
